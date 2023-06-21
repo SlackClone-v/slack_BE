68f04caa0e304c511e68d51dcdc6b26aa5ad7974
JUGGUM <gayoung801@naver.com>
2023-06-21 15:34:24 +0900

    Initial commit

---
+++ src/main/java/com/spring/instagramClone_BE/InstagramCloneBeApplication.java
@@ -0,0 +1,13 @@
+package com.spring.instagramClone_BE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InstagramCloneBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InstagramCloneBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spring/instagramClone_BE/InstagramCloneBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.instagramClone_BE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InstagramCloneBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
