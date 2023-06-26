2d3e34f14ac3750002de5f29ff9d2b6382cdbe61
JUGGUM <gayoung801@naver.com>
2023-06-26 17:07:39 +0900

    User Entity 생성

---
+++ src/main/java/com/spring/instagramClone_BE/User/User.java
@@ -0,0 +1,23 @@
+package com.spring.instagramClone_BE.User;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+@Data
+@AllArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue
+    private int id;
+
+    private String Name;
+    private String Email;
+    private String Password;
+
+}
+//api만드러서 url 넘기기
