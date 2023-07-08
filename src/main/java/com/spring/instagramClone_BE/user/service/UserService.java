1bdd0a44b3cf2ef77c8dd71afb4fb6d809e6574e
JUGGUM <gayoung801@naver.com>
2023-07-08 13:26:57 +0900

    findAllUsers 메소드 생성

--- src/main/java/com/spring/instagramClone_BE/user/service/UserService.java
+++ src/main/java/com/spring/instagramClone_BE/user/service/UserService.java
@@ -1,8 +1,19 @@
 package com.spring.instagramClone_BE.user.service;
 
 
+import com.spring.instagramClone_BE.user.entity.User;
+import com.spring.instagramClone_BE.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import javax.management.Notification;
+import java.util.List;
+
 @Service
+@RequiredArgsConstructor
 public class UserService {
+    private final UserRepository userRepository;
+    public List<User> findAll(){
+        return userRepository.findAll();
+    }
 }
